3b8018b7092cb2d582f3de97fec88c44cd2c64a8
Salvi03 <crissalvi57@gmail.com>
2020-09-06 21:04:06 +0200

    created array

---
+++ array.py
@@ -0,0 +1,34 @@
+class Array:
+    def __init__(self, length):
+        self.value = None
+        if length:
+            self.next = Array(length-1)
+
+    def set_value(self, pos, value):
+        if not pos:
+            self.value = value
+            return value
+
+        return self.next.set_value(pos-1, value)
+
+    def get_value(self, pos):
+        if not pos:
+            return self.value
+
+        return self.next.get_value(pos-1)
+
+
+if __name__ == "__main__":
+    array = Array(5)
+    array1 = Array(2)
+
+    array.set_value(0, array1)
+    array1.set_value(0, 6)
+
+    print(array.get_value(0).get_value(0))
+
+    """
+    array.set_value(2, 56)
+    array.set_value(0, 2)
+    print(f"{ array.get_value(0) } { array.get_value(2) }")
+    """
